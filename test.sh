f25fec8c7bf74ec3329614fadf82e8d102627c56
James Mills <1290234+prologic@users.noreply.github.com>
2019-12-05 22:07:10 +1000

    Simplify test.sh

--- test.sh
+++ test.sh
@@ -1,31 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-gui=0
-bridge=""
-
-while getopts gb: opt; do
-  case $opt in
-  g)
-    gui=1
-    ;;
-  b)
-    bridge=$OPTARG
-  esac
-done
-
-shift $((OPTIND - 1))
-
-echo "Booting..."
-echo
-
-if [ -n "$bridge" ]; then
-  network=-net bridge,br=br0 -net nic
-else
-  network=""
-fi
-
-if (( gui == 1 )); then
-  qemu-system-x86_64 -m 512M -boot d -cdrom mcl.iso -device virtio-rng-pci $network
-else
-  qemu-system-x86_64 -m 512M -boot d -nographic -cdrom mcl.iso -device virtio-rng-pci $network
-fi
+qemu-system-x86_64 \
+  -m 512M \
+  -boot d \
+  -cdrom mcl.iso \
+  -device virtio-rng-pci
